8a00215b85d8c74da8187428d2a665783d32ae2b
vvagap <agvva@ya.ru>
2023-01-29 23:12:57 +0700

    Boundary drawing + refactoring

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 """Module for Command Line Interface"""
 
 import argparse
+from os.path import splitext
 
 import matplotlib.image as img
 
@@ -18,12 +19,11 @@ def main():
     parser.add_argument(
         '-p', '--path',
         default='.',
-        help='path to dir for saving result image (default = current dir)',
+        help='path to dir to save compression result [default: current dir]',
     )
     parser.add_argument(
         '-n', '--name',
-        default='compressed',
-        help='name of the file for saving (default = compressed)',
+        help='name of the file to save compression result [default: [filename]-comp]',
     )
     parser.add_argument(
         '-b', '--bounds',
@@ -33,35 +33,53 @@ def main():
     )
     parser.add_argument(
         '-f', '--format',
-        choices=['png', 'gif'],
-        default='png',
-        help='format in which to save result file (default = png)'
+        choices=['img', 'anim'],
+        default='img',
+        help='format in which to save result file [default: img]'
     )
     parser.add_argument(
         '-r', '--ratio',
-        type=int, default=-1,
-        help='the higher the value, the lower the compression ratio (default = min compression)'
+        type=int, default=0,
+        help='the higher the value, the lower the compression ratio [default: 0]'
     )
 
     args = parser.parse_args()
 
+    name = args.name
+    if (not name):
+        name = splitext(args.filename)[0] + '-comp'
+
     print('Compression started!')
-    compress(args.filename, args.path, args.name, args.boundaries, args.format, args.ratio)
+    compress(
+        args.filename,
+        args.path,
+        name,
+        args.ratio,
+        args.format,
+        args.boundaries,
+    )
     print('Compression finished!')
 
 
-def compress(filename, path, name, boundaries, format, ratio):
+def compress(
+    filename: str,
+    path: str,
+    name: str,
+    ratio: int,
+    format: str,
+    boundaries: bool,
+):
     image = img.imread(filename)
 
     qtree = time_func(ImageQuadTree, False)(image)
-    
+
     if (ratio < 0 or ratio > qtree.leaf_level):
         ratio = qtree.leaf_level
 
-    if (format == 'png'):
-        qtree.save_image(ratio, boundaries, name, path)
+    if (format == 'img'):
+        qtree.save_image(ratio, path, name, boundaries)
     else:
-        qtree.save_gif(ratio, boundaries, name, path)
+        qtree.save_animation(ratio, path, name, boundaries)
 
 
 if __name__ == '__main__':
--- compress.py
+++ compress.py
@@ -20,193 +20,34 @@ from functools import reduce
 from util import time_func
 
 
-def quad_split(image):
+def quad_split(image: np.ndarray):
     half = np.array_split(image, 2)
     res = map(lambda x: np.array_split(x, 2, axis=1), half)
     return reduce(add, res)
 
 
-def quad_concat(tl, tr, bl, br):
+def quad_concat(tl: np.ndarray, tr: np.ndarray, bl: np.ndarray, br: np.ndarray):
     top = np.concatenate((tl, tr), axis=1)
     bottom = np.concatenate((bl, br), axis=1)
     return np.concatenate((top, bottom), axis=0)
 
 
-def calc_mean(image):
+def calc_mean_color(image: np.ndarray):
     return np.mean(image, axis=(0, 1))
 
 
-def calc_mean_color(split_image):
+def calc_mean_color_split(split_image: np.ndarray):
     return np.array(list(map(
-        lambda x: calc_mean(x), split_image))).astype(int)
+        lambda x: calc_mean_color(x), split_image)
+    )).astype(int)
 
 
 def is_equal(my_list):
     return all((x == my_list[0]).all() for x in my_list)
 
 
-def color_is_equal(image):
-    return all((x == colors[0]).all() for x in calc_mean_color(image))
-
-
-class ImageQuadTree:
-    def __init__(self, image, level=0, boundary=None, color_type=None, root=None):
-        if boundary is None:
-            boundary = Rectangle(0, 0, image.shape[1], image.shape[0])
-        self.boundary = boundary
-        self.level = level
-        if not color_type:
-            color_type = type(image[0][0][0].item())
-        self.mean = calc_mean(image).astype(color_type)
-        self.resolution = (image.shape[1], image.shape[0])
-        self.leaf = True
-
-        if root is None:
-            self.leaf_level = -1
-            root = self
-        if root.leaf_level < level:
-            root.leaf_level = level
-
-        if not is_equal(image):
-            split_img = quad_split(image)
-            self.leaf = False
-            arguments = [
-                [
-                    split_img[0],
-                    level + 1,
-                    Rectangle(boundary.get_left(),
-                              boundary.get_top(),
-                              split_img[0].shape[1],
-                              split_img[0].shape[0]),
-                    color_type
-                ],
-                [
-                    split_img[1],
-                    level + 1,
-                    Rectangle(boundary.get_left() + split_img[0].shape[1],
-                              boundary.get_top(),
-                              split_img[1].shape[1],
-                              split_img[1].shape[0]),
-                    color_type
-                ],
-                [
-                    split_img[2],
-                    level + 1,
-                    Rectangle(boundary.get_left(),
-                              boundary.get_top() + split_img[0].shape[0],
-                              split_img[2].shape[1],
-                              split_img[2].shape[0]),
-                    color_type
-                ],
-                [
-                    split_img[3],
-                    level + 1,
-                    Rectangle(boundary.get_left() + split_img[2].shape[1],
-                              boundary.get_top() + split_img[1].shape[0],
-                              split_img[3].shape[1],
-                              split_img[3].shape[0]),
-                    color_type
-                ]
-            ]
-            # if level == 0:
-            #     with ProcessPoolExecutor() as executor:
-            #         futures = [executor.submit(ImageQuadTree, *args, root)
-            #                    for args in arguments]
-            #         self.top_left = futures[0].result()
-            #         self.top_right = futures[1].result()
-            #         self.bottom_left = futures[2].result()
-            #         self.bottom_right = futures[3].result()
-            # else:
-            self.top_left = ImageQuadTree(*arguments[0], root)
-            self.top_right = ImageQuadTree(*arguments[1], root)
-            self.bottom_left = ImageQuadTree(*arguments[2], root)
-            self.bottom_right = ImageQuadTree(*arguments[3], root)
-
-    def get_image(self, level):
-        if self.leaf or self.level == level:
-            return np.tile(self.mean, (
-                self.boundary.get_height(),
-                self.boundary.get_width(),
-                1
-            ))
-        return quad_concat(
-            self.top_left.get_image(level),
-            self.top_right.get_image(level),
-            self.bottom_left.get_image(level),
-            self.bottom_right.get_image(level)
-        )
-
-    def draw_boundaries(self, ax, level):
-        if self.leaf or self.level == level:
-            line_width = 0.5
-            x1 = self.boundary.get_left()
-            x2 = self.boundary.get_right() - line_width
-            y1 = self.boundary.get_top()
-            y2 = self.boundary.get_bottom() - line_width
-            ax.plot([x1, x2, x2, x1, x1], [y1, y1, y2, y2, y1],
-                    c='black', lw=line_width)
-            return
-        self.top_left.draw_boundaries(ax, level)
-        self.top_right.draw_boundaries(ax, level)
-        self.bottom_left.draw_boundaries(ax, level)
-        self.bottom_right.draw_boundaries(ax, level)
-
-    @time_func
-    def save_image(
-        self,
-        level: int,
-        boundaries: bool = False,
-        name: str = 'compressed',
-        path: str = '.',
-    ):
-        if self.leaf:
-            return
-
-        fig = plt.figure()
-        plt.axis('off')
-        fig.axes[0].get_xaxis().set_visible(False)
-        fig.axes[0].get_yaxis().set_visible(False)
-        fig.set_size_inches((self.boundary.get_width() / 100,
-                             self.boundary.get_height() / 100))
-        ax = plt.Axes(fig, [0, 0, 1, 1])
-        ax.set_axis_off()
-        fig.add_axes(ax)
-
-        ax.imshow(self.get_image(level), aspect='equal')
-        
-        if boundaries and level < 10:
-            time_func(self.draw_boundaries)(ax, level)
-
-        plt.savefig(Path(f'{path}/{name}.png'))
-
-    @time_func
-    def save_gif(
-        self,
-        level: int,
-        boundaries: bool = False,
-        name: str = 'compressed',
-        path: str = '.',
-    ):
-        Path(f'tmp').mkdir(parents=True, exist_ok=True)
-
-        for i in range(level + 1):
-            self.save_image(i, boundaries, i, 'tmp')
-
-        frames = []
-
-        for i in range(level + 1):
-            frames.append(Image.open(f'tmp/{i}.png'))
-
-        frames[0].save(
-            Path(f'{name}.gif'),
-            save_all=True,
-            append_images=frames[1:],
-            optimize=True,
-            duration=1000,
-            loop=0
-        )
-
-        shutil.rmtree('tmp')
+def color_is_equal(image, colors):
+    return all((x == colors[0]).all() for x in calc_mean_color_split(image))
 
 
 class Rectangle:
@@ -239,3 +80,174 @@ class Rectangle:
                     self.get_right() < rect.get_left() or
                     self._top > rect.get_bottom() or
                     self.get_bottom() < rect.get_top())
+
+
+class ImageQuadTree:
+    def __init__(
+        self,
+        image: np.ndarray,
+        level: int = 0,
+        rect: Rectangle = None,
+        color_type: type = None,
+        root: ImageQuadTree = None
+    ):
+        if rect is None:
+            rect = Rectangle(0, 0, image.shape[1], image.shape[0])
+
+        self.rect = rect
+        self.level = level
+
+        if not color_type:
+            color_type = type(image[0][0][0].item())
+        self.mean_color = calc_mean_color(image).astype(color_type)
+
+        self.resolution = (image.shape[1], image.shape[0])
+        self.leaf = True
+
+        if root is None:
+            self.leaf_level = -1
+            root = self
+        if root.leaf_level < level:
+            root.leaf_level = level
+
+        if not is_equal(image):
+            split_img = quad_split(image)
+            self.leaf = False
+
+            left = rect.get_left()
+            top = rect.get_top()
+
+            arguments = [
+                [
+                    split_img[0],
+                    level + 1,
+                    Rectangle(left,
+                              top,
+                              split_img[0].shape[1],
+                              split_img[0].shape[0]),
+                    color_type,
+                    root
+                ],
+                [
+                    split_img[1],
+                    level + 1,
+                    Rectangle(left + split_img[0].shape[1],
+                              top,
+                              split_img[1].shape[1],
+                              split_img[1].shape[0]),
+                    color_type,
+                    root
+                ],
+                [
+                    split_img[2],
+                    level + 1,
+                    Rectangle(left,
+                              top + split_img[0].shape[0],
+                              split_img[2].shape[1],
+                              split_img[2].shape[0]),
+                    color_type,
+                    root
+                ],
+                [
+                    split_img[3],
+                    level + 1,
+                    Rectangle(left + split_img[2].shape[1],
+                              top + split_img[1].shape[0],
+                              split_img[3].shape[1],
+                              split_img[3].shape[0]),
+                    color_type,
+                    root
+                ]
+            ]
+
+            # if level == 0:
+            #     with ProcessPoolExecutor() as executor:
+            #         futures = [executor.submit(ImageQuadTree, *args)
+            #                    for args in arguments]
+            #         self.top_left = futures[0].result()
+            #         self.top_right = futures[1].result()
+            #         self.bottom_left = futures[2].result()
+            #         self.bottom_right = futures[3].result()
+            # else:
+            self.top_left = ImageQuadTree(*arguments[0])
+            self.top_right = ImageQuadTree(*arguments[1])
+            self.bottom_left = ImageQuadTree(*arguments[2])
+            self.bottom_right = ImageQuadTree(*arguments[3])
+
+    def get_square(self, level: int, boundaries: bool = False):
+        if self.leaf or self.level == level:
+            height = self.rect.get_height()
+            width = self.rect.get_width()
+
+            square = np.tile(self.mean_color, (
+            # ? Width -> height or height -> width?
+                height,
+                width,
+                1
+            ))
+
+            if boundaries:
+                square[0:height, width - 1] = [0, 0, 0]
+                square[height - 1, 0:width] = [0, 0, 0]
+
+            return square
+
+        return quad_concat(
+            self.top_left.get_square(level, boundaries),
+            self.top_right.get_square(level, boundaries),
+            self.bottom_left.get_square(level, boundaries),
+            self.bottom_right.get_square(level, boundaries)
+        )
+
+    @time_func
+    def save_image(
+        self,
+        level: int,
+        path: str = '.',
+        name: str = 'compressed',
+        boundaries: bool = False,
+    ):
+        fig = plt.figure()
+        plt.axis('off')
+        fig.axes[0].get_xaxis().set_visible(False)
+        fig.axes[0].get_yaxis().set_visible(False)
+        fig.set_size_inches((self.rect.get_width() / 100,
+                             self.rect.get_height() / 100))
+        ax = plt.Axes(fig, [0, 0, 1, 1])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+
+        ax.imshow(self.get_square(level, boundaries), aspect='equal')
+
+        plt.savefig(Path(f'{path}/{name}.png'))
+
+    @time_func
+    def save_animation(
+        self,
+        level: int,
+        path: str = '.',
+        name: str = 'compressed',
+        boundaries: bool = False,
+    ):
+        Path(f'tmp').mkdir(parents=True, exist_ok=True)
+
+        img_range = range(self.leaf_level, level - 1, -1)
+
+        for i in img_range:
+            self.save_image(i, 'tmp', i, boundaries)
+
+        frames = []
+
+        for i in img_range:
+            frames.append(Image.open(f'tmp/{i}.png'))
+
+        frames[0].save(
+            Path(f'{path}/{name}.gif'),
+            save_all=True,
+            append_images=frames[1:],
+            optimize=True,
+            duration=1000,
+            loop=0
+        )
+
+        shutil.rmtree('tmp')
